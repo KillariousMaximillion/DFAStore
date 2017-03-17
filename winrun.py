3e8b595991ac63a766f92534b8fa83246db9d536
Shannon Peterson <killarious.maximillion@gmail.com>
2017-03-17 10:59:13 -0500

    Added warning to top of winrun.py to warn that this a dev env only script.

--- winrun.py
+++ winrun.py
@@ -1,3 +1,8 @@
+'''
+This is a DEVElOPMENT ONLY script
+Use WSGI to run application in production on a production server.
+See runp.fcgi file for one possible solution
+'''
 #!flask/bin/python
 from app import app
 
