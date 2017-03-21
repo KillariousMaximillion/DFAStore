ad2b5203bca3c7e0ff5e8cf434d15a20d6a0cde0
Shannon Peterson <killarious.maximillion@gmail.com>
2017-03-21 18:52:48 -0500

    added web scaffolding for the DFAStore page

--- app/controllers.py
+++ app/controllers.py
@@ -1,6 +1,6 @@
 import requests
 
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from flask import json
 from wtforms import SelectField, StringField, PasswordField
 from wtforms.validators import DataRequired, Email
@@ -42,11 +42,50 @@ def GetMemberList(authtoken):
 
 	return memberlist
 
-class MembersForm(Form):
+class MembersForm(FlaskForm):
 	fields = []
 
 	members = SelectField('',choices=fields)
 	
-class SignInForm(Form):
+class SignInForm(FlaskForm):
 	email = StringField('Email', validators=[DataRequired(), Email()])
 	password = PasswordField('Password', validators=[DataRequired()])
+	
+class DFAStoreForm(FlaskForm):
+	dfaps = 0
+
+class CartForm(FlaskForm):
+	placeholder = 0
+	
+class ManagementForm(FlaskForm):
+	placeholder = 0
+	
+class Item(object):
+	ItemImage = None
+	ItemName = ''
+	ItemAmount = 0
+	ItemCost = 0
+	ItemStats = ''
+	
+	def __init__(self,ItemImage,ItemName,ItemAmount,ItemCost,ItemStats):
+		self.ItemImage = ItemImage
+		self.ItemName = ItemName
+		self.ItemAmount = ItemAmount
+		self.ItemCost = ItemCost
+		self.ItemStats = ItemStats
+			
+class ItemsList(object):
+	def getItems(self):
+		# query items from database here
+		Items = []
+		# iterate query of items and add the items list
+		
+		NewItem = Item(None, "Item1", "1", "1", "Stats1")
+		Items.append(NewItem)
+		NewItem = Item(None, "Item2", "2", "2", "Stats2")
+		Items.append(NewItem)
+		
+		if not Items:
+			return "No Items Currently Available in the DFA Store!"
+		return Items
+	
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
-from flask import render_template, redirect, url_for, session
+from flask import render_template, redirect, url_for, session, Markup, request
 from app import app
-from .controllers import MembersForm, SignInForm, AuthenticateLogin, GetMemberList, ValidToken
+from .controllers import MembersForm, SignInForm, DFAStoreForm, ManagementForm, CartForm, AuthenticateLogin, GetMemberList, ValidToken, ItemsList
 
 @app.route('/')
 @app.route('/index')
@@ -27,8 +27,45 @@ def signin():
 
 @app.route('/dfastore', methods=["GET", "POST"])
 def dfastore():
-	return render_template('dfastore.html', title='DFA Store')
+	if request.method == 'POST':
+		if 'managers_button' in request.form:
+			return redirect(url_for('management'))
+		elif 'cart_button' in request.form:
+			return redirect(url_for('cart'))
+		elif 'add_cart_button' in request.form:
+			return redirect(url_for('addtocart'))
+		
+	form = DFAStoreForm()
+	DFAPS = 'XXXX'		
+	ItemsTable = ItemsList.getItems(None)
+	CartButtonText = '&nbsp;&nbsp;X&nbsp;:&nbsp;Items &nbsp;&nbsp;&nbsp;X&nbsp;:&nbsp;DFAPs'
+	CartButton = Markup('<input style="height:100%; width:100%;color:white;background-color:limegreen;" name="cart_button" type="submit" value="' + CartButtonText + '">')
+	return render_template('dfastore.html', title='DFA Store', form=form, dfaps=DFAPS, cartbutton=CartButton, itemstable=ItemsTable)
 
+@app.route('/management', methods=["GET", "POST"])
+def management():
+	if request.method == 'POST':
+		if 'return_button' in request.form:
+			return redirect(url_for('dfastore'))
+		
+	form = ManagementForm()
+	return render_template('management.html', title='Management', form=form)
+			
+@app.route('/cart', methods=["GET", "POST"])
+def cart():
+	if request.method == 'POST':
+		if 'return_button' in request.form:
+			return redirect(url_for('dfastore'))
+		
+	form = CartForm()
+	return render_template('cart.html', title='CART', form=form)
+
+@app.route('/addtocart', methods=["GET", "POST"])
+def addtocart():
+	# items added to cart for the user
+	# update store data 
+	return redirect(url_for('dfastore'))
+			
 ### The members route is a test route
 @app.route('/members')
 def members():
